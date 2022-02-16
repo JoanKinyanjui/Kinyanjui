411ebb59703b34b54e5977279bc3d542af87c822
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2022-02-16 16:31:26 +0300

    hrefs

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -11,18 +11,18 @@ function Projects(){
             <br />
             <br />
      <div className='container1'>
-         <div className='container-item'>
+         <a href='https://amanicarwash.netlify.app/'><div className='container-item'>
 <img className='project-img' alt='carwash' src='/Images/Myportfolio.jpg'/>
 <p>Amani Carwash</p>
-         </div>
-         <div className='container-item'>
+         </div></a>
+         <a href='https://glowwke.com/'><div className='container-item'>
 <img className='project-img' alt='gloww'  src='/Images/project-gloww.jpg'/>
 <p>Gloww Cream Online Shop</p>
-         </div>
-         <div className='container-item'>
+         </div></a>
+         <a href='https://me2ucourier.netlify.app/'><div className='container-item'>
 <img className='project-img' alt='me2u'  src='/Images/project-me2u.jpg'/>
 <p>Me2u Courrier</p>
-         </div>
+         </div></a>
      </div>
         </div>
 
