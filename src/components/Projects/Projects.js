c6573f4e4100a34fe922686442dc086227fa617a
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2022-11-15 12:24:57 +0300

    ..

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -45,7 +45,9 @@ function Projects() {
   {/* Project4 */}
   <Grid item xs={12} md={6} className=' relative flex justify-center h-max  py-16'>
     <Box>
+      <a href='https://me2ucourier.netlify.app/'> 
 <img  src='/Images/project-me2u.jpg' className=' Big  w-5/6 h-mx md:h-96 mx-auto '/>
+</a>
 <div className=' w-full md:w-1/2 shadow-lg block mx-auto  absolute  bg-purple-200 text-gray-700    right-0 bottom-0 '>
 <p className='italic'>Me2U</p>
 <p>This is a Courier service website</p>
@@ -56,7 +58,9 @@ function Projects() {
   {/* 5 */}
   <Grid item xs={12} md={6} className=' relative flex justify-center h-max  py-16'>
     <Box>
+      <a href='https://whitelisting-dapp-five.vercel.app/'> 
 <img  src='/Images/whitelist.png' className='Big  w-5/6 h-mx md:h-96 mx-auto '/>
+</a>
 <div className=' w-full md:w-1/2 shadow-lg block mx-auto  absolute  bg-purple-200 text-gray-700    right-0 bottom-0 '>
 <p className='italic'>Whitelist Dapp</p>
 <p>This is a whitelisting Dapp ,that allows people to whitelist themselves so as to mint nft's from an nft marketplace 5mins before the public sale</p>
@@ -88,7 +92,9 @@ function Projects() {
   {/* 8 */}
   <Grid item xs={12} md={6} className='relative flex justify-center h-max  py-16'>
     <Box>
+<a href='https://code-for-impact-site-aum4.vercel.app/'>  
 <img  src='/Images/codeForImpact.png' className='Big  w-5/6 h-mx md:h-96 mx-auto '/>
+</a>
 <div className=' w-full md:w-1/2 shadow-lg block mx-auto  absolute  bg-purple-200 text-gray-700    right-0 bottom-0 '>
 <p className='italic'>Code For Impact</p>
 <p>This is a website for code for impact.An organisation that seeks to teach code for the benefit of people who do not have access to resources</p>
